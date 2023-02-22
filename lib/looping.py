54427acd8c916179e021510bc1cc7bf054e8b5c9
Forrest Jones <forrestjones2010@gmail.com>
2023-02-22 10:58:45 -0600

    GDC#5

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -+ 1
+    print("Happy New Year!")
+        
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def square_integers(lst):
+    squared_lst = [x**2 for x in lst if isinstance(x, int)]
+    return squared_lst
+    print(square_integers([1, 2, 3, 4, 5]))
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
